23d435611ed8694160da7fcd51f6d22603c4429a
ldelvillar <ldelvillarvazquez@gmail.com>
2026-07-02 10:49:12 +0200

    feat(frontend): update globe icon

--- frontend/src/assets/Globe.tsx
+++ frontend/src/assets/Globe.tsx
@@ -3,16 +3,20 @@ import type { SVGProps } from 'react';
 export default function GlobeIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
       viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth={1.9}
-      strokeLinecap="round"
-      strokeLinejoin="round"
       {...props}
     >
-      <circle cx="12" cy="12" r="9" />
-      <path d="M3.5 12h17M12 3c2.4 2.4 2.4 15.6 0 18M12 3c-2.4 2.4-2.4 15.6 0 18" />
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={1.5}
+        d="M12 21a9 9 0 1 0 0-18m0 18a9 9 0 1 1 0-18m0 18c2.761 0 3.941-5.163 3.941-9S14.761 3 12 3m0 18c-2.761 0-3.941-5.163-3.941-9S9.239 3 12 3M3.5 9h17m-17 6h17"
+      ></path>
     </svg>
   );
 }
